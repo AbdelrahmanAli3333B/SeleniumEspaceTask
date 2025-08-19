5b38d32ea3d0e91bea37c82eb5c75c54c816ea4c
original <abdelrahman.ali3333@gmail.com>
2025-08-19 20:31:25 +0200

    19-8-2025 9:30 PM

--- src/test/java/OrangeHRM.java
+++ src/test/java/OrangeHRM.java
@@ -8,6 +8,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class OrangeHRM {
 
@@ -29,12 +30,15 @@ public class OrangeHRM {
         Assert.assertTrue(driver.findElement(By.xpath("//h6[normalize-space()='Dashboard']")).isDisplayed(), "Dashboard");
     }
 
-    public static void SearchForUser (String username){
+    public static void SearchForUser (String targetedUsername){
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         By adminScreen = By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a");
         By userSearch = By.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']");
         driver.findElement(adminScreen).click();
-        driver.findElement(userSearch).sendKeys(username + Keys.ENTER);
+        driver.findElement(userSearch).sendKeys(targetedUsername + Keys.ENTER);
+        List<WebElement> rows = driver.findElements(By.xpath("//div[@class='oxd-table-body']//div[@role='row']"));
+        System.out.println("Number of result rows: " + rows.size());
+        Assert.assertEquals(rows.size(),1);
     }
 
 
@@ -44,20 +48,20 @@ public class OrangeHRM {
             openURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         }
 
+        //2. Login using Username: Admin & Password: admin123
         @Test(priority = 1)
         public void loginTest(){
-            //2. Login using Username: Admin & Password: admin123
             Login("Admin", "admin123");
         }
 
-
         //3. Click the Admin tab on the left sidebar
-
         //4. Search using the username for the admin user
-        //String targetedUsername = "fosho1163";
-       // SearchForUser(targetedUsername);
-
         //5. Assert that the number of results, the username, the user role, and the status are correct
+        @Test(priority = 2, dependsOnMethods = {"loginTest"})
+        public void searchForUser(){
+            String targetedUsername = "user9599";
+            SearchForUser(targetedUsername);
+        }
 
         //6. Click the Delete icon (trash bin) on the table
 
