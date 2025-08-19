b0d47c416298d59b45f23c8c4b37f787097ddb1d
original <abdelrahman.ali3333@gmail.com>
2025-08-19 20:02:44 +0200

    19-8-2025 9:02 PM

--- src/test/java/OrangeHRM.java
+++ src/test/java/OrangeHRM.java
@@ -1,6 +1,11 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -21,18 +26,36 @@ public class OrangeHRM {
         driver.findElement(usernameInput).sendKeys(username);
         driver.findElement(passwordInput).sendKeys(password);
         driver.findElement(submitButton).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//h6[normalize-space()='Dashboard']")).isDisplayed(), "Dashboard");
     }
 
-    public static void main(String[] args) {
-        //1. Navigate to https://opensource-demo.orangehrmlive.com
-        openURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    public static void SearchForUser (String username){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        By adminScreen = By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a");
+        By userSearch = By.xpath("//div[@class='oxd-input-group oxd-input-field-bottom-space']//div//input[@class='oxd-input oxd-input--active']");
+        driver.findElement(adminScreen).click();
+        driver.findElement(userSearch).sendKeys(username + Keys.ENTER);
+    }
 
-        //2. Login using Username: Admin & Password: admin123
-        Login("Admin", "admin123");
+
+        //1. Navigate to https://opensource-demo.orangehrmlive.com
+        @BeforeClass
+        public void setUp(){
+            openURL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        }
+
+        @Test(priority = 1)
+        public void loginTest(){
+            //2. Login using Username: Admin & Password: admin123
+            Login("Admin", "admin123");
+        }
+
 
         //3. Click the Admin tab on the left sidebar
 
         //4. Search using the username for the admin user
+        //String targetedUsername = "fosho1163";
+       // SearchForUser(targetedUsername);
 
         //5. Assert that the number of results, the username, the user role, and the status are correct
 
@@ -41,7 +64,7 @@ public class OrangeHRM {
         //7. Assert that the admin user deletion is not allowed (either nothing happens, or an error appears)
 
     }
-}
+
 
 
 
