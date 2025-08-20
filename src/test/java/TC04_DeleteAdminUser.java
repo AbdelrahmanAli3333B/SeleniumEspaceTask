d1199beaef46b5eb6dd5cec58217d8c7de14905b
AbdelrahmanPioneers <142295073+AbdelrahmanPioneers@users.noreply.github.com>
2025-08-20 22:49:40 +0300

    20-8-2024 10:49 PM

---
+++ src/test/java/TC04_DeleteAdminUser.java
@@ -0,0 +1,24 @@
+import Pages.P02_HomePage;
+import Pages.P03_AdminPage;
+import Utilities.DataUtil;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+
+public class TC04_DeleteAdminUser extends Tests.BaseTest{
+
+    P02_HomePage homePage;
+    P03_AdminPage adminPage;
+
+    @Test
+    public void deleteAdminUser() throws FileNotFoundException {
+        login(DataUtil.getJsonData("myTestData","username")
+                ,DataUtil.getJsonData("myTestData","password"));
+        homePage = new P02_HomePage(driver);
+        homePage.clickOnAdminTab();
+        adminPage = new P03_AdminPage(driver);
+        adminPage.enterUsernameInSearchArea(DataUtil.getJsonData("myTestData","targetedUsername")).clickSearchButton();
+
+    }
+}
