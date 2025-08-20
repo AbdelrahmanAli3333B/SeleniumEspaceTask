211a621817301ee3ae66aafedcde062a108f8548
abdelrahman <abdelrahman.pioneers@gmail.com>
2025-08-20 13:38:17 +0300

    20-8-2025 1:38PM

--- src/main/java/Pages/P01_LoginPage.java
+++ src/main/java/Pages/P01_LoginPage.java
@@ -1,4 +1,29 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
+
 public class P01_LoginPage {
+    private WebDriver driver;
+    private final By usernameInput = By.name("username");
+    private final By passwordInput = By.name("password");
+    private final By loginButton = By.cssSelector("button[type='submit']");
+
+    public P01_LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void enterUsername(String usernameText){
+        driver.findElement(usernameInput).sendKeys(usernameText);
+    }
+
+    public void enterPassword(String passwordText){
+        driver.findElement(passwordInput).sendKeys(passwordText);
+    }
+
+    public void clickLoginButton(){
+        driver.findElement(loginButton).click();
+    }
 }
