92408bc43ae8f4f101ba7eb4b6ab8cc7b010435a
abdelrahman <abdelrahman.pioneers@gmail.com>
2025-08-20 13:58:29 +0300

    20-8-2025 1:58PM

--- src/test/java/TC01_LoginTest.java
+++ src/test/java/TC01_LoginTest.java
@@ -1,2 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import Pages.P01_LoginPage;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class TC01_LoginTest {
+    private WebDriver driver;
+    P01_LoginPage loginPage;
+
+    @BeforeMethod
+
+    public void setupDriver(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void validLogin(){
+        loginPage = new P01_LoginPage(driver);
+        loginPage.enterUsername("Admin");
+        loginPage.enterPassword("admin123");
+        loginPage.clickLoginButton();
+    }
+
+    @AfterMethod
+    public void quit(){
+        driver.quit();
+    }
 }
